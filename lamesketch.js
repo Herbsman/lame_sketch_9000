c8c36ab903e37879a2f906cabcfd0de1fa7cc345
Tilen Cizerl <kalfrik@gmail.com>
2015-01-31 03:54:21 +0100

    Laid the groundwork. The grid is set up and dynamic. \n Still need to add the buttons for clearing the grid and changing interaction modes(different color, mouse-trailing...); \n Perhaps include a snapshot function of sketch? Also, I need to add the option for custom grid sizes. The default is 16x16. \n Should go to bed now.

---
+++ lamesketch.js
@@ -0,0 +1,23 @@
+//Appends the divs as soon as the document is loaded (ready)
+$(document).ready(function(){
+	var	divNum = 16;
+	var grid = Math.pow(divNum, 2);		//given number = number of rows and collumns to conjure up.
+	var $contHeight = $("#contentWrapper").height();	//grabs the wrapper's height (the bare value, no units) + stores it
+	var $contWidth = $("#contentWrapper").width();		//same as above, but for width
+	var divHeight = $contHeight / divNum;		//divides the wrapper's height by # of divs - this will be the div height.
+	var divWidth = $contWidth / divNum;			//same as above, but for width
+	for(i = 0; i < grid; i++){
+		$("#contentWrapper").append("<div class='sketchDivs'></div>");
+	};
+		$(".sketchDivs").css("height", divHeight);
+		$(".sketchDivs").css("width", divWidth);
+	$(".sketchDivs").hover(function(){
+		$(this).css("opacity", "-=0.2");
+	});
+});
+
+
+
+
+
+
