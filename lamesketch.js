4da556563a2419d563e4dd3f73f89a8e4625d1c1
Tilen Cizerl <kalfrik@gmail.com>
2015-02-01 01:38:20 +0100

    Added default and random color modes

--- lamesketch.js
+++ lamesketch.js
@@ -1,62 +1,101 @@
 
-
 //Appends the divs as soon as the document is loaded (ready)
 function init(divNum){
 	$(document).ready(function(){
-		if(divNum === undefined || divNum === null){  //sets the grid dimensions to 40 if the variable is empty.
-			divNum = 10;
+		if(divNum === undefined || divNum === null){  //sets the grid dimensions to the default if the variable is empty.
+			divNum = 10; //the
 		}
 
 		//Variables 
 		grid = Math.pow(divNum, 2);		//given number = number of rows and collumns to conjure up.
-		$contHeight = $("#contentWrapper").height();	//grabs the wrapper's height (the bare value, no units) + stores it
-		$contWidth = $("#contentWrapper").width();		//same as above, but for width
-		divHeight = $contHeight / divNum;		//divides the wrapper's height by # of divs - this will be the div height.
-		divWidth = $contWidth / divNum;			//same as above, but for width
-
+		divHeight = $("#contentWrapper").height() / divNum;		//divides the wrapper's height by # of divs - this will be the div height.
+		divWidth = $("#contentWrapper").width() / divNum;			//same as above, but for width
 
 		//functions
 
 		gridSetup();
 
+		$("p").html("Select a mode!")
+
+
+
+		//different modes
+		$(document).on("click", "#defMode", function(){  //random color mode engaged when proper button is clicked
+			defaultMode();
+			$("p").html("Get drawing!");
+		});
+
+		$(document).on("click", "#rndmColor", function(){  //random color mode engaged when proper button is clicked
+			randomColor();
+			$("p").html("Get drawing!");
+		});
+
 		//button actions
 
 		$(document).on("click", "#buttonRedo", function(){
 			gridNew();
+			$("p").html("Select a mode!")
 		});
 
 		$(document).on("click", "#buttonWipe", function(){
 			gridWipe();
+			$("p").html("Select a mode!")
 		});
 
 	});
 };
 
-function gridSetup(){		//this one sets up the grid
+//this one sets up the grid
+function gridSetup(){
 
-sktchDiv = ".sketchDivs"; 	// I define this out of the loop, so the stored element isn't looked up each time (faster execution).
+sktchDivs = ".sketchDivs"; 	// I define this out of the loop, so the stored element isn't looked up each time (faster execution).
 
 for(i = 0; i < grid; i++){
 		$("#contentWrapper").append("<div class='sketchDivs'></div>");
 	};
-		$(sktchDiv).css("height", divHeight);
-		$(sktchDiv).css("width", divWidth);
-	$(sktchDiv).hover(function(){
+		$(sktchDivs).css("height", divHeight);
+		$(sktchDivs).css("width", divWidth);
+}
+
+
+//wipes the grid, asks for input, then calls init() to set up new grid dimensions.
+function gridNew(){
+	$(".sketchDivs").remove();
+	gridDim = prompt("Enter a number for the grid's dimensions.\n(Higher numbers (n > 100) might take a while!)\n");
+	init(gridDim);
+}
+
+//Wipes grid, restores initial div class opacity.
+
+function gridWipe(){
+	$(".sketchDivs").css("opacity", "0");	//Should make this work across the different modes that I plan to implement.
+	$(".sketchDivs").css("background-color", "transparent");
+}							//Right now, it just works for the default mode.
+
+
+//mode functions
+
+function defaultMode(){		//the default mode
+	$(sktchDivs).css("opacity", "0");
+	$(sktchDivs).hover(function(){
 		$(this).css("opacity", "+=0.2");
+		$(this).css("background-color", "black");
 	});
 }
 
-function gridNew(){			//wipes the grid, asks for input, then calls init() to set up new grid dimensions.
-	$(".sketchDivs").remove();
-	gridDim = prompt("Enter a number for the grid's dimensions.");
-	init(gridDim);
-	console.log("I was exectued fully!");
+function randomColor(){		//random colors mode
+	gridWipe();
+	function randomizer(){  	//returns a number between 0 and 255 (for rgb values)
+		rndm = Math.floor(Math.random() * 255);
+		rndmA = Math.random()+0.3; // ensures the number the alpha will be at least 0.3
+		return rndm;
+	}
+	$(sktchDivs).css("opacity", "1");
+	$(sktchDivs).hover(function(){	// color is RGBa (Red, Green, Blue, alpha)
+		randomColorRgb = "rgb("+randomizer()+","+randomizer()+","+randomizer()+","+rndmA+")"; //Applies the randomizer to each of the RGB values
+		$(this).css("background-color", randomColorRgb);
+	});
 }
-
-function gridWipe(){		//Wipes grid, restores initial class settings.
-	$(".sketchDivs").css("opacity", "0");	//Should make this work across the different modes that I plan to implement.
-}							//Right now, it just works for the default mode.
-
 
 
 init();
